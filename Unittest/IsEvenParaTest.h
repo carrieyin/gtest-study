de3e42c7fe90a76cc292ebf9449ecde6442d8382
carrieyin <695685009@qq.com>
2021-01-24 21:29:41 +0800

    gmock使用

---
+++ Unittest/IsEvenParaTest.h
@@ -0,0 +1,8 @@
+#ifndef ISEVEN_PARAM_TEST
+#define ISEVEN_PARAM_TEST
+
+#include "gtest/gtest.h"
+
+
+
+#endif // !ISEVEN_PARAM_TEST
--- Unittest/TestAUnitTest.cpp
+++ Unittest/TestAUnitTest.cpp
@@ -1,6 +1,7 @@
 #include "TestA.h"
 #include "TestAmock.h"
 
+
 TEST_F(TestA, isEven1)
 {
     EXPECT_EQ(false, isEven(1));
@@ -16,6 +17,23 @@ TEST_F(TestA, getNumber1)
     EXPECT_EQ(10, getNum());
 }
 
+class IsEvenParaTest : public ::testing::TestWithParam<int>
+{
+private:
+    /* data */
+public:
+};
+
+/*TEST_P(IsEvenParaTest, isEven)
+{
+    TestA a;
+    int n = GetParam();
+    EXPECT_TRUE(a.isEven(n));
+}
+
+INSANTIATE_TEST_CASE_P(TrueReturn, IsEvenParaTest, testing::Values(2, 4, 6, 8));
+*/
+//以下报错gmock必须基于多态的虚函数
 // TEST_F(TestA, mockTestA)
 // {
 //    MockTestA a;
